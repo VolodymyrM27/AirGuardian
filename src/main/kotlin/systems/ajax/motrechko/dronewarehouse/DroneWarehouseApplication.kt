0a88029248e22a593f47e4392867d3f76f6ff2ef
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-08-30 16:31:50 +0300

    Initial commit

---
+++ src/main/kotlin/systems/ajax/motrechko/dronewarehouse/DroneWarehouseApplication.kt
@@ -0,0 +1,11 @@
+package systems.ajax.motrechko.dronewarehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DroneWarehouseApplication
+
+fun main(args: Array<String>) {
+    runApplication<DroneWarehouseApplication>(*args)
+}
---
+++ src/test/kotlin/systems/ajax/motrechko/dronewarehouse/DroneWarehouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package systems.ajax.motrechko.dronewarehouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DroneWarehouseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
