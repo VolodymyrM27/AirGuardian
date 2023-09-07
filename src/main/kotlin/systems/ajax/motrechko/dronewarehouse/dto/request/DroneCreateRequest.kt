b4a340457de7e3a1f74ea6de42b3248b8579f574
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-07 10:27:34 +0300

    remove  @param:JsonProperty annotation

--- src/main/kotlin/systems/ajax/motrechko/dronewarehouse/dto/request/DroneCreateRequest.kt
+++ src/main/kotlin/systems/ajax/motrechko/dronewarehouse/dto/request/DroneCreateRequest.kt
@@ -4,19 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import systems.ajax.motrechko.dronewarehouse.enums.DroneType
 import systems.ajax.motrechko.dronewarehouse.model.Drone
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 
 data class DroneCreateRequest @JsonCreator constructor(
     val model: String,
     val type: List<DroneType> = emptyList(),
     val speed: Double,
     val weight: Double,
-    @param:JsonProperty("number_of_propellers")
-    @get:JsonProperty("number_of_propellers")
     val numberOfPropellers: Int,
-    @param:JsonProperty("load_capacity")
-    @get:JsonProperty("load_capacity")
     val loadCapacity: Double,
     val cost: Double
 )
