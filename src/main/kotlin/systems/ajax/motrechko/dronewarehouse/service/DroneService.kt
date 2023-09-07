50784c7cc84c0da3fbd0008e1f8ea48fe3f0b914
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-07 10:28:14 +0300

    remove findDroneById method

--- src/main/kotlin/systems/ajax/motrechko/dronewarehouse/service/DroneService.kt
+++ src/main/kotlin/systems/ajax/motrechko/dronewarehouse/service/DroneService.kt
@@ -2,20 +2,20 @@ package systems.ajax.motrechko.dronewarehouse.service
 
 import org.springframework.stereotype.Service
 import systems.ajax.motrechko.dronewarehouse.dto.response.DroneResponse
+import systems.ajax.motrechko.dronewarehouse.enums.DroneType
 import systems.ajax.motrechko.dronewarehouse.exception.DroneNotFoundException
 import systems.ajax.motrechko.dronewarehouse.model.Drone
 import systems.ajax.motrechko.dronewarehouse.repository.DroneRepository
 
 @Service
 class DroneService(private val droneRepository: DroneRepository) {
+
     fun getAllDrones(): List<Drone> = droneRepository.findAll()
+
     fun getDroneById(id: String): Drone =
         droneRepository.findById(id).orElseThrow { DroneNotFoundException("Drone with id $id not found") }
-
-    fun findDroneById(id: String): Drone? = droneRepository.findById(id).orElse(null)
 
     fun createDrone(drone: Drone): Drone = droneRepository.save(drone)
 
     fun deleteDroneById(id: String) = droneRepository.deleteById(id)
-
 }
