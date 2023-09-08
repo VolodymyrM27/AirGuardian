ebc2f545a596a0d619317960e700c39a46e7e5bc
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-08 13:37:28 +0300

    Updated Drone DTO`s

--- src/main/kotlin/systems/ajax/motrechko/dronewarehouse/dto/request/DroneCreateRequest.kt
+++ src/main/kotlin/systems/ajax/motrechko/dronewarehouse/dto/request/DroneCreateRequest.kt
@@ -1,18 +1,37 @@
 package systems.ajax.motrechko.dronewarehouse.dto.request
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import systems.ajax.motrechko.dronewarehouse.enums.DroneSize
+import systems.ajax.motrechko.dronewarehouse.enums.DroneStatus
 import systems.ajax.motrechko.dronewarehouse.enums.DroneType
 import systems.ajax.motrechko.dronewarehouse.model.Drone
 
 
 data class DroneCreateRequest @JsonCreator constructor(
+    @NotBlank
     val model: String,
     val type: List<DroneType> = emptyList(),
+    @NotBlank
+    @Min(0)
     val speed: Double,
+    @NotBlank
+    @Min(0)
     val weight: Double,
+    @NotBlank
+    @Min(0)
     val numberOfPropellers: Int,
+    @NotBlank
+    @Min(0)
     val loadCapacity: Double,
-    val cost: Double
+    @NotBlank
+    @Min(0)
+    val cost: Double,
+    val status: DroneStatus,
+    val batteryLevel: Double,
+    val size: DroneSize,
+    val maxFlightAltitude: Double
 )
 
 fun DroneCreateRequest.toEntity() = Drone(
@@ -22,5 +41,9 @@ fun DroneCreateRequest.toEntity() = Drone(
     weight = weight,
     numberOfPropellers = numberOfPropellers,
     loadCapacity = loadCapacity,
-    cost = cost
+    cost = cost,
+    status = status,
+    batteryLevel = batteryLevel,
+    size = size,
+    maxFlightAltitude = maxFlightAltitude
 )
--- src/main/kotlin/systems/ajax/motrechko/dronewarehouse/dto/response/DroneResponse.kt
+++ src/main/kotlin/systems/ajax/motrechko/dronewarehouse/dto/response/DroneResponse.kt
@@ -1,6 +1,7 @@
 package systems.ajax.motrechko.dronewarehouse.dto.response
 
-import org.bson.types.ObjectId
+import systems.ajax.motrechko.dronewarehouse.enums.DroneSize
+import systems.ajax.motrechko.dronewarehouse.enums.DroneStatus
 import systems.ajax.motrechko.dronewarehouse.enums.DroneType
 import systems.ajax.motrechko.dronewarehouse.model.Drone
 
@@ -12,7 +13,11 @@ data class DroneResponse(
     val weight: Double,
     val numberOfPropellers: Int,
     val loadCapacity: Double,
-    val cost: Double
+    val cost: Double,
+    val status: DroneStatus,
+    val batteryLevel: Double,
+    val size: DroneSize,
+    val maxFlightAltitude: Double
 )
 
 fun Drone.toResponse() = DroneResponse(
@@ -23,7 +28,11 @@ fun Drone.toResponse() = DroneResponse(
     weight = weight,
     numberOfPropellers = numberOfPropellers,
     loadCapacity = loadCapacity,
-    cost = cost
+    cost = cost,
+    status = status,
+    batteryLevel = batteryLevel,
+    size = size,
+    maxFlightAltitude = maxFlightAltitude
 )
 fun List<Drone>.toResponse(): List<DroneResponse> {
     return this.map { drone ->
@@ -35,7 +44,11 @@ fun List<Drone>.toResponse(): List<DroneResponse> {
             weight = drone.weight,
             numberOfPropellers = drone.numberOfPropellers,
             loadCapacity = drone.loadCapacity,
-            cost = drone.cost
+            cost = drone.cost,
+            status = drone.status,
+            batteryLevel = drone.batteryLevel,
+            size = drone.size,
+            maxFlightAltitude = drone.maxFlightAltitude
         )
     }
 }
