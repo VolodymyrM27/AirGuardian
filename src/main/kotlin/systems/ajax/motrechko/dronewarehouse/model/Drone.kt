d207806862b8402a8291a1a598acffa9c09eb3ae
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-08 13:11:03 +0300

    Updated Drone.kt  - Added field`s Drone.status, Drone.batteryLevel, Drone.flightHistory, Drone.size, Drone.maxFlightAltitude

--- src/main/kotlin/systems/ajax/motrechko/dronewarehouse/model/Drone.kt
+++ src/main/kotlin/systems/ajax/motrechko/dronewarehouse/model/Drone.kt
@@ -3,6 +3,8 @@ package systems.ajax.motrechko.dronewarehouse.model
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import systems.ajax.motrechko.dronewarehouse.enums.DroneSize
+import systems.ajax.motrechko.dronewarehouse.enums.DroneStatus
 import systems.ajax.motrechko.dronewarehouse.enums.DroneType
 
 @Document("drones")
@@ -15,5 +17,10 @@ data class Drone(
     val weight: Double = 0.0,
     val numberOfPropellers: Int = 0,
     val loadCapacity: Double = 0.0,
-    val cost: Double = 0.0
+    val cost: Double = 0.0,
+    val status: DroneStatus = DroneStatus.INACTIVE,
+    val batteryLevel: Double = 100.0,
+    val flightHistory: List<FlightRecord> = emptyList(),
+    val size: DroneSize = DroneSize.MEDIUM,
+    val maxFlightAltitude: Double = 0.0
 )
