b8ddd6fab6b494ff187b4139708c8aa85ab8f9df
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-12 13:39:42 +0300

    - Added DeliveryStatus.WAITING_AVAILABLE_DRONES - Added new enum class DeliveryItemType - Updated DeliveryItem ( added DeliveryItem.deliveryItemType, DeliveryItem.weight ) - Updated DeliveryOrder ( DeliveryOrder.deliveryCoordinates, DeliveryOrder.deliveryDrone )

---
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/enums/DeliveryItemType.kt
@@ -0,0 +1,5 @@
+package systems.ajax.motrechko.airguardian.enums
+
+enum class DeliveryItemType {
+ MEDICAL, FOOD, COMMERCIAL_CARGO
+}
--- src/main/kotlin/systems/ajax/motrechko/airguardian/model/DeliveryItem.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/model/DeliveryItem.kt
@@ -1,6 +1,13 @@
 package systems.ajax.motrechko.airguardian.model
 
+import systems.ajax.motrechko.airguardian.enums.DeliveryItemType
+
 data class DeliveryItem(
     val productName: String,
-    val quantity: Int
+    val deliveryItemType: DeliveryItemType,
+    val quantity: Int,
+    val weight: Double
 )
+
+fun List<DeliveryItem>.getTotalWeight(): Double = this.sumOf { it.weight }
+
