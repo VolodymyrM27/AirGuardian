e4face7c35281a1b7ad78f56c1fa7f238544bb1b
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-12 13:47:48 +0300

    forgot to add a file to the b8ddd6fa commit

--- src/main/kotlin/systems/ajax/motrechko/airguardian/model/DeliveryOrder.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/model/DeliveryOrder.kt
@@ -2,13 +2,17 @@ package systems.ajax.motrechko.airguardian.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import systems.ajax.motrechko.airguardian.enums.DeliveryStatus
 
+@Document("delivery_order")
 data class DeliveryOrder(
     @Id
     val id: ObjectId = ObjectId(),
     val customerName: String = "",
     val deliveryAddress: String = "",
-    val items: List<DeliveryItem> = emptyList(),
-    val status: DeliveryStatus = DeliveryStatus.PENDING
+    val deliveryCoordinates: Coordinates,
+    var items: List<DeliveryItem> = emptyList(),
+    var status: DeliveryStatus = DeliveryStatus.PENDING,
+    var deliveryDrone: List<Drone>? = emptyList()
 )
