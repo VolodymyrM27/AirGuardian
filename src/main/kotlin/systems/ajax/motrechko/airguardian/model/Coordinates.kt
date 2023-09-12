81c53c627f37c7e919bf4edfb738f7ba40e530f5
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-12 11:04:07 +0300

    Added method for random generated Coordinates

--- src/main/kotlin/systems/ajax/motrechko/airguardian/model/Coordinates.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/model/Coordinates.kt
@@ -1,6 +1,23 @@
 package systems.ajax.motrechko.airguardian.model
 
+import kotlin.random.Random
+
 data class Coordinates(
     val latitude: Double,
     val longitude: Double
-)
+) {
+    companion object {
+        private const val MIN_LATITUDE = -90.0
+        private const val MAX_LATITUDE = 90.0
+        private const val MIN_LONGITUDE = -180.0
+        private const val MAX_LONGITUDE = 180.0
+    }
+
+    fun generateRandomCoordinates(): Coordinates {
+        val randomLatitude = Random.nextDouble(MIN_LATITUDE, MAX_LATITUDE)
+        val randomLongitude = Random.nextDouble(MIN_LONGITUDE, MAX_LONGITUDE)
+
+        return Coordinates(randomLatitude, randomLongitude)
+    }
+}
+
