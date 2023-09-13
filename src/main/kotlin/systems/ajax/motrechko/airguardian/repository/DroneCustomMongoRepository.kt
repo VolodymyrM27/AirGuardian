9725248d2b2103bd4d4b20d80d2f00a9569ad22e
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-13 19:25:33 +0300

    Added DroneCustomRepository interface Added implementation of this interface in DroneCustomMongoRepository.kt  - Added implementation of updateDroneInfo

---
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/repository/DroneCustomMongoRepository.kt
@@ -0,0 +1,33 @@
+package systems.ajax.motrechko.airguardian.repository
+
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
+import org.springframework.stereotype.Repository
+import systems.ajax.motrechko.airguardian.model.Drone
+
+@Repository
+class DroneCustomMongoRepository(
+    private val mongoTemplate: MongoTemplate,
+) : DroneCustomRepository {
+    override fun updateDroneInfo(drone: Drone) {
+        val query = Query()
+            .addCriteria(Criteria.where("_id").`is`(drone.id))
+        val update = Update()
+            .set("model", drone.model)
+            .set("type", drone.type)
+            .set("speed", drone.speed)
+            .set("weight", drone.weight)
+            .set("numberOfPropellers", drone.numberOfPropellers)
+            .set("loadCapacity", drone.loadCapacity)
+            .set("cost", drone.cost)
+            .set("status", drone.status)
+            .set("batteryLevel", drone.batteryLevel)
+            .set("flightHistory", drone.flightHistory)
+            .set("size", drone.size)
+            .set("maxFlightAltitude", drone.maxFlightAltitude)
+
+        mongoTemplate.updateFirst(query, update, Drone::class.java)
+    }
+}
