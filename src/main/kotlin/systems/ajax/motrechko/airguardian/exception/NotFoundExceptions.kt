2bd69336a87990760cbe6d87e7579c1c50e74fed
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-18 16:17:10 +0300

    - Moving NotFound exceptions`s to one file   - Rewrite droneNotFound method to universal method for all notFoundExceptions

--- src/main/kotlin/systems/ajax/motrechko/airguardian/exception/NotFoundExceptions.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/exception/NotFoundExceptions.kt
@@ -1,3 +1,5 @@
 package systems.ajax.motrechko.airguardian.exception
 
 class DroneNotFoundException(message: String) : Exception(message)
+class DeliveryOrderNotFoundException (message: String) : Exception(message)
+
