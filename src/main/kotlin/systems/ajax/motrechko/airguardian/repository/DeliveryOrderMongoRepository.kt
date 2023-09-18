5d264798fc033d8a7b0c34166420d29650c2f730
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-09-18 17:20:27 +0300

    Added new methods and their implementation - findOrderByStatus - deleteByID - findOrdersByDroneId - findOrdersByUserId - update

--- src/main/kotlin/systems/ajax/motrechko/airguardian/repository/DeliveryOrderMongoRepository.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/repository/DeliveryOrderMongoRepository.kt
@@ -3,8 +3,11 @@ package systems.ajax.motrechko.airguardian.repository
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Repository
+import systems.ajax.motrechko.airguardian.enums.DeliveryStatus
 import systems.ajax.motrechko.airguardian.enums.DroneStatus
+import systems.ajax.motrechko.airguardian.model.DeliveryOrder
 import systems.ajax.motrechko.airguardian.model.Drone
 
 @Repository
@@ -16,5 +19,42 @@ class DeliveryOrderMongoRepository(
             .addCriteria(Criteria.where("status").`is`(DroneStatus.ACTIVE))
             .addCriteria(Criteria.where("loadCapacity").gte(totalWeight))
         return mongoTemplate.find(query, Drone::class.java)
+    }
+
+    override fun findOrderByStatus(status: DeliveryStatus): List<DeliveryOrder> {
+        val query = Query()
+            .addCriteria(Criteria.where("status").`is`(status.name))
+        return mongoTemplate.find(query, DeliveryOrder::class.java)
+    }
+
+    override fun deleteByID(id: String) {
+        val query = Query()
+            .addCriteria(Criteria.where("id").`is`(id))
+        val update = Update()
+            .set("status", DeliveryStatus.DELETED)
+        mongoTemplate.updateFirst(query, update, DeliveryOrder::class.java)
+    }
+
+    override fun findOrdersByDroneId(droneID: String): List<DeliveryOrder> {
+        val query = Query(Criteria.where("deliveryDroneIDs").`in`(droneID))
+        return mongoTemplate.find(query, DeliveryOrder::class.java)
+    }
+
+    override fun findOrdersByUserId(userID: String): List<DeliveryOrder> {
+        val query = Query(Criteria.where("deliveryDroneIDs").`in`(userID))
+        return mongoTemplate.find(query, DeliveryOrder::class.java)
+    }
+
+    override fun update(order: DeliveryOrder) {
+        val query = Query()
+            .addCriteria(Criteria.where("_id").`is`(order.id))
+        val update = Update()
+            .set("customerName", order.customerName)
+            .set("items", order.items)
+            .set("status", order.status)
+            .set("deliveryAddress", order.deliveryAddress)
+            .set("deliveryCoordinates", order.deliveryCoordinates)
+            .set("deliveryDroneIDs", order.deliveryDroneIDs)
+        mongoTemplate.updateFirst(query, update, DeliveryOrder::class.java)
     }
 }
