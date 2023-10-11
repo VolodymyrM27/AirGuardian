f863226a19596461d8c8d7c6c68c5690aab7bcbf
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-10-11 11:21:35 +0300

    Updated enum to pretty format code

--- src/main/kotlin/systems/ajax/motrechko/airguardian/enums/DroneStatus.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/enums/DroneStatus.kt
@@ -1,5 +1,9 @@
 package systems.ajax.motrechko.airguardian.enums
 
 enum class DroneStatus {
-    INACTIVE, ACTIVE, CHARGING, NEED_TO_CHARGE, BUSY
+    INACTIVE,
+    ACTIVE,
+    CHARGING,
+    NEED_TO_CHARGE,
+    BUSY
 }
--- src/main/kotlin/systems/ajax/motrechko/airguardian/enums/EmergencyEventType.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/enums/EmergencyEventType.kt
@@ -1,5 +1,8 @@
 package systems.ajax.motrechko.airguardian.enums
 
 enum class EmergencyEventType {
-    ROBBERY, FIRE, SHOOTING, OTHER
+    ROBBERY,
+    FIRE,
+    SHOOTING,
+    OTHER
 }
