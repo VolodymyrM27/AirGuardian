9611fb5cc824e8ab30a9793f31f00a7d0c226793
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-10-12 14:33:02 +0300

    Fixed BIIIIIIIG bug

--- src/main/kotlin/systems/ajax/motrechko/airguardian/service/DeliveryOrderService.kt
+++ src/main/kotlin/systems/ajax/motrechko/airguardian/service/DeliveryOrderService.kt
@@ -9,6 +9,7 @@ import systems.ajax.motrechko.airguardian.enums.DeliveryStatus
 import systems.ajax.motrechko.airguardian.enums.DroneStatus
 import systems.ajax.motrechko.airguardian.exception.DeliveryOrderNotFoundException
 import systems.ajax.motrechko.airguardian.model.DeliveryOrder
+import systems.ajax.motrechko.airguardian.model.Drone
 import systems.ajax.motrechko.airguardian.repository.DeliveryOrderMongoReactiveRepository
 import systems.ajax.motrechko.airguardian.repository.DeliveryOrderRepository
 import systems.ajax.motrechko.airguardian.repository.DroneRepository
@@ -23,37 +24,22 @@ class DeliveryOrderService(
     fun createNewOrder(order: DeliveryOrder): Mono<DeliveryOrder> {
         return droneLogisticsService.findAvailableDrones(order.items)
             .collectList()
-            .filter { availableDrones -> availableDrones.isNotEmpty() }
-            .flatMap {availableDrones ->
-                droneLogisticsService.initializeOrderForDelivery(order, availableDrones)
-                deliveryOrderRepository.save(order)
-                    .thenReturn(order)
-                    .doOnSuccess {
-                        logger.info(
-                            "find {} drones for delivery new order with id {}",
-                            availableDrones.size,
-                            order.id
-                        )
-                    }
+            .flatMap { availableDrones ->
+                if (availableDrones.isNotEmpty()) {
+                    println("!!!!!SwitchIfEmpty but in if")
+                    initializeOrderForDeliveryAndSave(order, availableDrones)
+                } else {
+                    println("SwitchIfEmpty but in if")
+                    setWaitingStatusAndSave(order)
+                }
             }
-            .switchIfEmpty(Mono.defer {
-                order.status = DeliveryStatus.WAITING_AVAILABLE_DRONES
-                deliveryOrderRepository.save(order)
-                    .thenReturn(order)
-                    .doOnSuccess { logger.info("no free drones were found for prayer with id {}", order.id) }
-            })
     }
 
-    fun getInfoAboutOrderByID(id: String): Mono<DeliveryOrder> =
-        deliveryOrderRepository
-            .findById(id)
-            .switchIfEmpty(Mono.error(DeliveryOrderNotFoundException("Order with $id not found")))
+    fun getInfoAboutOrderByID(id: String): Mono<DeliveryOrder> = deliveryOrderRepository.findById(id)
+        .switchIfEmpty(Mono.error(DeliveryOrderNotFoundException("Order with $id not found")))
 
-
-    fun findDeliveryOrderByStatus(deliveryStatus: DeliveryStatus): Flux<DeliveryOrder> =
-        deliveryOrderCustomRepository
-            .findOrderByStatus(deliveryStatus)
-
+    fun findAllDeliveryOrdersByStatus(deliveryStatus: DeliveryStatus): Flux<DeliveryOrder> =
+        deliveryOrderCustomRepository.findOrderByStatus(deliveryStatus)
 
     fun deleteByID(id: String) = deliveryOrderCustomRepository.deleteByID(id)
 
@@ -62,16 +48,45 @@ class DeliveryOrderService(
 
     fun complete(id: String): Mono<DeliveryOrder> {
         return getInfoAboutOrderByID(id)
-            .flatMap { order ->
-                order.status = DeliveryStatus.DELIVERED
-                droneRepository.updateManyDronesStatus(
-                    order.deliveryDroneIds,
-                    DroneStatus.ACTIVE
+            .flatMap { setOrderAsDelivered(it) }
+    }
+
+    private fun setOrderAsDelivered(order: DeliveryOrder): Mono<DeliveryOrder> {
+        order.status = DeliveryStatus.DELIVERED
+        return droneRepository.updateManyDronesStatus(
+            order.deliveryDroneIds, DroneStatus.ACTIVE
+        )
+            .then(deliveryOrderCustomRepository.save(order))
+            .thenReturn(order)
+    }
+
+    private fun setWaitingStatusAndSave(order: DeliveryOrder): Mono<DeliveryOrder> {
+       return Mono.defer {
+           println("From SwitchIfEmpty")
+           order.status = DeliveryStatus.WAITING_AVAILABLE_DRONES
+           println("$order")
+           deliveryOrderRepository.save(order)
+               .thenReturn(order)
+               .doOnSuccess { logger.info("no free drones were found for prayer with id {}", order.id) }
+       }
+    }
+
+    private fun initializeOrderForDeliveryAndSave(
+        order: DeliveryOrder,
+        availableDrones: MutableList<Drone>
+    ): Mono<DeliveryOrder> {
+        return droneLogisticsService.initializeOrderForDelivery(order, availableDrones)
+            .flatMap {
+                println(order)
+                deliveryOrderRepository.save(order)
+            }
+            .doOnSuccess {
+                logger.info(
+                    "find {} drones for delivery new order with id {}", availableDrones.size, order.id
                 )
-                    .flatMap { deliveryOrderCustomRepository.save(order) }
-                    .thenReturn(order)
             }
     }
+
 
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(DeliveryOrderService::class.java)
