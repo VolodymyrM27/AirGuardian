c771cae6d76c83b44215b657702fd32821695846
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-10-22 18:35:53 +0300

    Add NatsControllerBeanPostProcessor

---
+++ airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/beanPostProcessor/NatsControllerBeanPostProcessor.kt
@@ -0,0 +1,31 @@
+package systems.ajax.motrechko.airguardian.beanPostProcessor
+
+import org.springframework.beans.factory.config.BeanPostProcessor
+import org.springframework.stereotype.Component
+import com.google.protobuf.GeneratedMessageV3
+import io.nats.client.Connection
+import systems.ajax.motrechko.airguardian.controller.nats.NatsController
+
+
+@Component
+class NatsControllerBeanPostProcessor(
+    private val connection: Connection
+) : BeanPostProcessor {
+
+    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+        if (bean is NatsController<*, *>) {
+            initializeNatsController(bean, connection)
+        }
+        return bean
+    }
+
+    fun <RequestT : GeneratedMessageV3, ResponseT : GeneratedMessageV3>
+            initializeNatsController(controller: NatsController<RequestT, ResponseT>, connection: Connection) {
+        connection.createDispatcher { message ->
+            val parsedData = controller.parser.parseFrom(message.data)
+            controller
+                .handle(parsedData)
+                .subscribe { response -> connection.publish(message.replyTo, response.toByteArray()) }
+        }.subscribe(controller.subject)
+    }
+}
