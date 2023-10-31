0c4d539efa464eea129c2ca7c1009985e9bf57f8
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-10-31 17:31:39 +0200

    Add Mapper to proto file

---
+++ airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/mapper/DroneBatteryApplicationMapper.kt
@@ -0,0 +1,11 @@
+package systems.ajax.motrechko.airguardian.mapper
+
+import systems.ajax.motrechko.airguardian.commonresponse.application.drone_battery_charging_application.proto.DroneBatteryChargingApplication
+import systems.ajax.motrechko.airguardian.model.BatteryApplication
+
+fun BatteryApplication.toProto(): DroneBatteryChargingApplication {
+    return DroneBatteryChargingApplication.newBuilder()
+        .setServiceMessage(this.serviceMessage)
+        .setDroneId(this.droneId)
+        .build()
+}
