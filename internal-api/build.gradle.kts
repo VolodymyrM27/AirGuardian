351709c2553275444dab3177764d9058c1728069
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-11-02 12:56:35 +0200

    Add dependency for reactive gRPC

--- internal-api/build.gradle.kts
+++ internal-api/build.gradle.kts
@@ -1,9 +1,26 @@
-dependencies {
-    implementation("com.google.protobuf:protobuf-java:3.24.3")
-}
+import com.google.protobuf.gradle.id
 
 protobuf {
     protoc {
         artifact = "com.google.protobuf:protoc:3.24.3"
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.59.0"
+        }
+
+        id("reactor-grpc") {
+            artifact = "com.salesforce.servicelibs:reactor-grpc:1.2.4"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("reactor-grpc")
+            }
+        }
     }
 }
