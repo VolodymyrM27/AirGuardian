6fc62ae4e8f037d4407a233a5ffdcb7899e33b64
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-11-05 22:59:11 +0200

    Add more reactive approach

--- airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/kafka/DroneChargingApplicationKafkaProducer.kt
+++ airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/kafka/DroneChargingApplicationKafkaProducer.kt
@@ -2,6 +2,7 @@ package systems.ajax.motrechko.airguardian.kafka
 
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 import reactor.kafka.sender.KafkaSender
 import reactor.kafka.sender.SenderRecord
 import reactor.kotlin.core.publisher.toMono
@@ -12,21 +13,32 @@ import systems.ajax.motrechko.airguardian.output.pubsub.application.drone_batter
 class DroneChargingApplicationKafkaProducer(
     private val droneChargingApplicationKafkaEventSender: KafkaSender<String, DroneBatteryChargingApplicationEvent>
 ) {
-    fun sendBatteryChargingApplication(batteryApplicationProto: DroneBatteryChargingApplication) {
-        val droneBatteryChargingApplicationEvent = DroneBatteryChargingApplicationEvent.newBuilder()
+    fun sendBatteryChargingApplication(batteryApplicationProto: DroneBatteryChargingApplication): Mono<Unit> {
+        return Mono.fromSupplier { createDroneBatteryChargingApplicationEvent(batteryApplicationProto) }
+            .flatMap {
+                droneChargingApplicationKafkaEventSender.send(createSenderRecord(batteryApplicationProto, it)).next()
+            }
+            .thenReturn(Unit)
+    }
+
+    private fun createSenderRecord(
+        batteryApplicationProto: DroneBatteryChargingApplication,
+        droneBatteryChargingApplicationEvent: DroneBatteryChargingApplicationEvent
+    ) = SenderRecord.create(
+        ProducerRecord(
+            "drone-battery-charging-application",
+            batteryApplicationProto.droneId,
+            droneBatteryChargingApplicationEvent
+        ),
+        null
+    ).toMono()
+
+
+    private fun createDroneBatteryChargingApplicationEvent(
+        batteryApplicationProto: DroneBatteryChargingApplication
+    ): DroneBatteryChargingApplicationEvent =
+        DroneBatteryChargingApplicationEvent.newBuilder()
             .setApplication(batteryApplicationProto)
             .build()
-
-        val record = SenderRecord.create(
-            ProducerRecord(
-                "drone-battery-charging-application",
-                batteryApplicationProto.droneId,
-                droneBatteryChargingApplicationEvent
-            ),
-            null
-        )
-
-        droneChargingApplicationKafkaEventSender.send(record.toMono()).subscribe()
-    }
 }
 
