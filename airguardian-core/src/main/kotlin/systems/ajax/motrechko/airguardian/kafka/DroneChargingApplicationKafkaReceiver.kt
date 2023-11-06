5d24bde2577a16d68e98927ca35d5411245e59ab
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-11-06 12:07:59 +0200

    Add kafka receiver

---
+++ airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/kafka/DroneChargingApplicationKafkaReceiver.kt
@@ -0,0 +1,28 @@
+package systems.ajax.motrechko.airguardian.kafka
+
+import jakarta.annotation.PostConstruct
+import org.springframework.stereotype.Component
+import reactor.core.scheduler.Schedulers
+import reactor.kafka.receiver.KafkaReceiver
+import systems.ajax.motrechko.airguardian.controller.nats.BatteryDroneChargingApplicationEventNatsController
+import systems.ajax.motrechko.airguardian.output.pubsub.application.drone_battery_charging_application.proto.DroneBatteryChargingApplicationEvent
+
+@Component
+class DroneChargingApplicationKafkaReceiver(
+    private val droneChargingApplicationKafkaConsumer: KafkaReceiver<String, DroneBatteryChargingApplicationEvent>,
+    private val batteryDroneChargingApplicationEventNatsController:
+    BatteryDroneChargingApplicationEventNatsController<DroneBatteryChargingApplicationEvent>
+) {
+    @PostConstruct
+    fun subscribeToEvents() {
+        droneChargingApplicationKafkaConsumer.receiveAutoAck()
+            .flatMap { fluxRecord ->
+                fluxRecord
+                    .map {
+                        batteryDroneChargingApplicationEventNatsController.publishEvent(it.value().application)
+                    }
+            }
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe()
+    }
+}
