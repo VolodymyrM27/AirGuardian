a28938431ea369b48855a6c58b1c135b64b9966c
Volodymyr Motrechko <volodymyrmotrechko@Noutbuk-Volodymyr.local>
2023-11-06 12:07:28 +0200

    Added configuration for receiver

--- airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/configuration/KafkaConfiguration.kt
+++ airguardian-core/src/main/kotlin/systems/ajax/motrechko/airguardian/configuration/KafkaConfiguration.kt
@@ -1,14 +1,21 @@
 package systems.ajax.motrechko.airguardian.configuration
 
 import com.google.protobuf.GeneratedMessageV3
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializerConfig
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import reactor.kafka.receiver.KafkaReceiver
+import reactor.kafka.receiver.ReceiverOptions
 import reactor.kafka.sender.KafkaSender
 import reactor.kafka.sender.SenderOptions
+import systems.ajax.motrechko.airguardian.internalapi.KafkaTopic
 import systems.ajax.motrechko.airguardian.output.pubsub.application.drone_battery_charging_application.proto.DroneBatteryChargingApplicationEvent
 
 
@@ -21,6 +28,30 @@ class KafkaConfiguration(
     fun kafkaSenderDroneBatteryChargingApplicationEvent(): KafkaSender<String, DroneBatteryChargingApplicationEvent> =
         createKafkaSender(producerProperties())
 
+    @Bean
+    fun droneChargingApplicationKafkaReceiver(): KafkaReceiver<String, DroneBatteryChargingApplicationEvent> {
+        val customProperties: MutableMap<String, Any> = mutableMapOf(
+            KafkaProtobufDeserializerConfig.SPECIFIC_PROTOBUF_VALUE_TYPE to
+                    DroneBatteryChargingApplicationEvent::class.java.name
+        )
+        return createKafkaReceiver(
+            consumerProperties(customProperties),
+            KafkaTopic.CHARGING_EVENT,
+            "drone-application-group"
+        )
+    }
+
+    private fun <T : GeneratedMessageV3> createKafkaReceiver(
+        properties: MutableMap<String, Any>,
+        topic: String,
+        groupId: String
+    ): KafkaReceiver<String, T> {
+        properties[ConsumerConfig.GROUP_ID_CONFIG] = groupId
+        val options =
+            ReceiverOptions.create<String, T>(properties).subscription(setOf(topic))
+        return KafkaReceiver.create(options)
+    }
+
     private fun <T : GeneratedMessageV3> createKafkaSender(properties: MutableMap<String, Any>):
             KafkaSender<String, T> =
         KafkaSender.create(SenderOptions.create(properties))
@@ -31,6 +62,19 @@ class KafkaConfiguration(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaProtobufSerializer::class.java.name,
+            "schema.registry.url" to schemaRegistryUrl
+        )
+        baseProperties.putAll(customProperties)
+        return baseProperties
+    }
+
+    private fun consumerProperties(
+        customProperties: MutableMap<String, Any> = mutableMapOf()
+    ): MutableMap<String, Any> {
+        val baseProperties: MutableMap<String, Any> = mutableMapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaProtobufDeserializer::class.java.name,
             "schema.registry.url" to schemaRegistryUrl
         )
         baseProperties.putAll(customProperties)
